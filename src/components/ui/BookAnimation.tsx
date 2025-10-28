b306cb20390c2602892603cd579ea9e6a59c305b
evertonestevao <everton.efdsilva@gmail.com>
2025-10-28 17:57:00 -0300

    bookanimation add

--- src/components/ui/BookAnimation.tsx
+++ src/components/ui/BookAnimation.tsx
@@ -10,7 +10,7 @@ export default function BookLoader() {
   > | null>(null);
 
   useEffect(() => {
-    fetch("/animations/book.json")
+    fetch("/animations/bookanimation.json")
       .then((res) => res.json())
       .then(setAnimationData)
       .catch((err) => console.error("Erro ao carregar animação:", err));
