18a534857fe086047410c50b29f6213408bb7749
evertonestevao <everton.efdsilva@gmail.com>
2025-11-21 18:40:38 -0300

    home screen settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center from-gray-50 to-white dark:from-gray-950 dark:to-black p-8">
-        <main className="flex flex-col items-center gap-8 text-center max-w-md flex-1 justify-center">
-          <div className="w-64 h-64">
+      <div className="min-h-screen flex flex-col from-gray-50 to-white dark:from-gray-950 dark:to-black p-8">
+        <main className="flex-1 flex flex-col items-center gap-8 text-center max-w-md mx-auto mt-8">
+          <div className="w-32 h-32 sm:w-64 sm:h-64">
             <BookLoader />
           </div>
 
@@ -28,28 +28,26 @@ export default function Home() {
           </p>
 
           <div className="w-full flex flex-col items-center mt-4">
-            {/* Linha dos botões */}
             <div className="flex flex-col sm:flex-row gap-4 w-full justify-center">
               <LoginButton />
               <Link href="/biblia">
                 <Button
                   variant="outline"
                   className="w-full sm:w-auto flex items-center gap-3 justify-center
-                border-gray-300 dark:border-gray-700"
+            border-gray-300 dark:border-gray-700"
                 >
                   Bíblia online
                 </Button>
               </Link>
             </div>
 
-            <div className="mt-4">
+            <div className="mt-8">
               <ThemeToggle />
             </div>
           </div>
         </main>
 
-        {/* Rodapé */}
-        <footer className=" text-sm text-gray-500 dark:text-gray-400">
+        <footer className="text-sm text-gray-500 text-center dark:text-gray-400">
           Criado com ❤️ para inspirar, compartilhar e fortalecer o hábito da
           leitura da Palavra de Deus.
         </footer>
